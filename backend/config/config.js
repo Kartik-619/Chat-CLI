6cd7bb2713d7eda72a5dde7800314193634db8ed
Kartik Sharma <poojan4604@gmail.com>
2025-08-11 21:48:42 +0530

    userschema and database connection

---
+++ backend/config/config.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+MongoUrl='mongodb+srv://<db_username>:<db_password>@cluster0.mtcedjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const connectDB=async ()=>{
+    try{
+        await mongoose.connect(MongoUrl);
+        console.log('Database connected');
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+};
+module.exports=connectDB;

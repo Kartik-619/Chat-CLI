a83c7944da7a9c580ed668efbfc3cd6ff6b130ba
Kartik Sharma <poojan4604@gmail.com>
2025-08-13 22:33:43 +0530

    port address corrected

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const connectDB=require('./config/config');
-
-const app=express();
 const port=4500;
+const app=express();
+
 
 //middlewares
 app.use(express.json());
